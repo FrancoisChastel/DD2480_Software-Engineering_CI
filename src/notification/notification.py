34e6d2f7912d5cfd22241167983025f97385b381
Bmritt <bmritt@gmail.com>
2018-02-04 14:08:50 +0100

    issue #4 added smtp capability on new gmail address to send notifications of test results

--- src/notification/notification.py
+++ src/notification/notification.py
@@ -1,1 +1,28 @@
+import smtplib
 
+def send_notifications(message):
+
+	fromaddr = 'DD2480.CI@gmail.com'	#from addr
+	toaddrs  = 'DD2480.CI@gmail.com'	#to addr
+	msg = "\r\n".join([					#message with headers for from, to, and subject
+	  "From: DD2480.CI@gmail.com",
+	  "To: DD2480.CI@gmail.com",
+	  "Subject: Test Results",
+	  "",
+	  message
+	  ])
+	username = 'DD2480.CI@gmail.com'	#log into smtp
+	password = 'DD2480CI'
+	server = smtplib.SMTP('smtp.gmail.com:587')	#init smtp server
+	server.ehlo()
+	server.starttls()
+	server.login(username,password)
+	server.sendmail(fromaddr, toaddrs, msg)
+	server.quit()
+
+
+if __name__ == "__main__":
+	send_notifications("Hello World!")		#current test, should send an email to DD2480.CI@gmail.com with subject "Test Results" and message "Hello World!"
+
+
+	
