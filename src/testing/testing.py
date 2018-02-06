7b20905f331bf39c0c96af90505f3f9369d4dbe1
Anu Devarmanai <adevar2@illinois.edu>
2018-02-07 00:29:03 +0100

    update tests issue #3

--- src/testing/testing.py
+++ src/testing/testing.py
@@ -5,11 +5,8 @@ import pytest
 import communication
 from compilation import compilation
 
-#contract: test for errors when downloading the commit
-def test_downloader_1():
-    assert 1 == 1
 
-#test for compilation errors using state value where 0 is error, 1 is no error, and 2 is compilation warning
+#contract: test for compilation errors using state value where 0 is error, 1 is no error, and 2 is compilation warning
 def test_compilation_1():
     c = communication.Result()
     compilation.to_compile(c)
@@ -18,13 +15,13 @@ def test_compilation_1():
         print("Compilation warning!")
     print(c.compiling_messages)
 
-#test for attribute errors during compilation process
+#contract: test for attribute errors during compilation process
 def test_compilation_2():  # needs an assert statement
     commun = communication.Result()
     with pytest.raises(AttributeError):
         compilation.to_compile(commun)
 
-#test that notifications are correctly sent via email with the correct compilation and testing results
+#contract: test that notifications are correctly sent via email with the correct compilation and testing results
 def test_notification_1():
     assert 3 == 3
 
