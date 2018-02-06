1307dc367733d6ae5d697fd56cb370983b5d37fc
Anu Devarmanai <adevar2@illinois.edu>
2018-02-06 23:30:39 +0100

    doc to_compile function for issue #2

--- src/compilation/compilation.py
+++ src/compilation/compilation.py
@@ -1,6 +1,6 @@
 from pylint import epylint as lint
 
-
+#compile code in the repo and collect the compilation messages in the communication object
 def to_compile(communication):
     (pylint_stdout, pylint_stderr) = lint.py_run("/".join([communication.location, "src"]), return_std=True)
     communication.compiling_messages = pylint_stdout
