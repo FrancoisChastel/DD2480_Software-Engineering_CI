81e8c558ff203e501b0352d5e568084f9f7e4520
Anu Devarmanai <adevar2@illinois.edu>
2018-02-06 20:17:08 +0100

    added compilation test

--- src/testing/testing.py
+++ src/testing/testing.py
@@ -9,7 +9,16 @@ from notification import notification
 def test_downloader():
     assert 1 == 1 
 
-def test_compilation():
+def test_compilation_1():
+    c = communication.Result()
+    compilation.to_compile(c)
+    assert c.state != 0, "Compilation failed" #where is state updated?
+    if (c.state == 2):
+        print("Compilation warning!")
+    print(c.compiling_messages)
+ 
+    
+def test_compilation_2():
     commun = communication.Result()
     with pytest.raises(AttributeError):
         compilation.to_compile(commun)
