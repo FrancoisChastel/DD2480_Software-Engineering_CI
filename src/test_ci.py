5c4004fbafb0c0a6a4a356970351f9c6ae0a460c
shenmemingzi <470323747@qq.com>
2018-02-07 09:42:39 +0800

    bug fix: for test

--- src/test_ci.py
+++ src/test_ci.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import pytest
+from communication import communication
+from downloader import downloader
+from compilation import compilation
+from notification import notification
 
 def test_compilation_1():
     #contract: to_compile enable to throw a AttributeError when parameter contains nothing
