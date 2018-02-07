14a750b47d791083056cf58bfa7b8a57ffba3838
François Chastel <francois@chastel.co>
2018-02-07 01:23:27 +0100

    issue #4, feature: configuration file for the personalized message that will be sent my mail

---
+++ src/notification/configs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ER_CPL_MESSAGE = "Error triggered while compiling with error code : %s\n" \
+                 "\t* Commit author : %s\n" \
+                 "\t* Commit id     : %s\n" \
+                 "\t* Source url    : %s\n" \
+                 "==================================================================\n" \
+                 "\t* Compiler logs : \n" \
+                 "%s\n"
+
+ER_TST_MESSAGE = "Error triggered while compiling with error code : %s\n" \
+                 "\t* Commit author : %s\n" \
+                 "\t* Commit id     : %s\n" \
+                 "\t* Source url    : %s\n" \
+                 "==================================================================\n" \
+                 "\t* Testing logs : \n" \
+                 "%s\n"
+
+WRN_CPL_MESSAGE = "Warning triggered while compiling and/or testing with error code : %s\n" \
+                  "\t* Commit author : %s\n" \
+                  "\t* Commit id     : %s\n" \
+                  "\t* Source url    : %s\n" \
+                  "==================================================================\n" \
+                  "\t* Compiler logs : \n" \
+                  "%s\n" \
+                  "==================================================================\n" \
+                  "\t* Tester logs : \n" \
+                  "%s\n"
+
+SCC_MESSAGE = "Compilation and testing successfully made !" \
+              "\t* Commit author : %s\n" \
+              "\t* Commit id     : %s\n" \
+              "\t* Source url    : %s\n"
