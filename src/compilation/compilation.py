a7a30b71bb75eb670b346975a4e8f7e82416bd1b
François Chastel <francois@chastel.co>
2018-02-07 01:21:51 +0100

    issue #2, fix: of the communication compatibility for compilation and adding output of the static analysis to the communication object

--- src/compilation/compilation.py
+++ src/compilation/compilation.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import communication
 from pylint import epylint as lint
 
 
-def to_compile(communication):
-    (pylint_stdout, pylint_stderr) = lint.py_run("/".join([communication.location, "src"]), return_std=True)
-    communication.compiling_messages = pylint_stdout
+def to_compile(result):
+    (pylint_stdout, pylint_stderr) = lint.py_run("/".join([result.location, "src"]), return_std=True)
+
+    result.compiling_messages = pylint_stdout.buf
+
+    if "error" in result.compiling_messages:
+        result.state = communication.State.COMPILING_FAILED
+    elif "warning" in result.compiling_messages:
+        result.state = communication.State.COMPILING_WARNED
+    else:
+        result.state = communication.State.COMPILING_SUCCEED
