334f8675b46a09f6ffeb571782c9c1a40268dca7
shenmemingzi <470323747@qq.com>
2018-02-07 09:38:06 +0800

    issue #15: add one test case for test

--- src/test_ci.py
+++ src/test_ci.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 
-def test_downloader_1():
-    assert 1 == 1
+def test_compilation_1():
+    #contract: to_compile enable to throw a AttributeError when parameter contains nothing
+    c = communication.Result()
+    with pytest.raises(AttributeError):
+        compilation.to_compile(c)
 
 
 def test_notification_1():
