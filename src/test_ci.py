a38dda38372086c4dc06d519f06cf5acaa88a076
Bmritt <bmritt@gmail.com>
2018-02-09 19:26:26 +0100

    Issue #15 added test for existence of correct email, but does not work with pytest. Details in comments of test

--- src/test_ci.py
+++ src/test_ci.py
@@ -63,3 +63,45 @@ def test_notification_3():
 #    c.state = communication.State.TEST_SUCCEED
 #    with pytest.raises(AttributeError): #assert when not get an AttributeError
 #        m = notification.get_message(c)
+
+
+#TO FIX
+#WORKS WHEN RUN ALONE, BUT NOT WHEN RUNNING WITH PYTEST
+
+# def test_notification_5():
+#     # contract: email sent contains all fields set from result structure
+
+#     #populate result structure
+#     c = communication.Result()
+#     c.state = communication.State.COMPILING_FAILED
+#     c.author = 'Brian'
+#     c.commit = 'test_commit_number'
+#     c.url_repo = 'www.test.com'
+#     c.compiling_messages = 'fail'
+
+#     #send email
+#     notification.send_notifications(c)
+
+#     #Login to email server
+#     username = 'DD2480.CI@gmail.com'    
+#     password = 'DD2480CI'
+#     mail = imaplib.IMAP4_SSL("imap.gmail.com")
+#     mail.login(username,password)
+#     mail.select('inbox')
+
+#     #select mailbox
+#     type, data = mail.search(None,'ALL')
+#     mail_ids = data[0]
+
+#     #select email
+#     id_list = mail_ids.split()   
+#     latest_email_id = int(id_list[-1])
+
+#     #check for all fields
+#     typ, data = mail.fetch(latest_email_id, '(RFC822)' )
+#     if isinstance(data[0], tuple):
+#         resp = data[0][1]
+#         msg = email.message_from_string(resp)
+#         msg = str(msg)
+#         assert msg.find('Brian') != -1 and msg.find('test_commit_number') != -1 and msg.find('www.test.com') != -1 and \
+#         msg.find('fail') != -1 and msg.find('COMPILING_FAILED') != -1
